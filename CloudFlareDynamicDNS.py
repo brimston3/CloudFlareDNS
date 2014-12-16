ad75e8dd0ed9113c204dec3903f3a652b6263035
tw <tw@hephaestus>
2014-12-16 17:43:18 -0500

    From the because-map-reduce-makes-everything-more-complicated department. (completely untested, of course)

--- CloudFlareDynamicDNS.py
+++ CloudFlareDynamicDNS.py
@@ -1,36 +1,41 @@
+#!/usr/bin/env python
+
 import os,re
 import urllib
 import sys
+import json
 
-cloudflareEmail="EMAIL@EXAMPLE.COM"	#CF Login Email Address
-cloudflareAPIkey="cloudflareapikeylowercase"	#API Key as shown on https://www.cloudflare.com/my-account
-baseDomain='yourdomain.com'	#Domain Name as shown on https://www.cloudflare.com/my-websites.html
-recordType='A'   	#See "Type" Column on https://www.cloudflare.com/dns-settings?z=example.com
-recordName='yoursubdomain'	#See "Name" Column on https://www.cloudflare.com/dns-settings?z=example.com
+cloudflareEmail="EMAIL@EXAMPLE.COM" #CF Login Email Address
+cloudflareAPIkey="cloudflareapikeylowercase"    #API Key as shown on https://www.cloudflare.com/my-account
+baseDomain='yourdomain.com' #Domain Name as shown on https://www.cloudflare.com/my-websites.html
+recordType='A'      #See "Type" Column on https://www.cloudflare.com/dns-settings?z=example.com
+recordName='yoursubdomain'  #See "Name" Column on https://www.cloudflare.com/dns-settings?z=example.com
 
 #Find the CloudFlare ID of your (sub)domain based on the recordName and recordType
 data={'a': 'rec_load_all','tkn': cloudflareAPIkey,'email': cloudflareEmail,'z': baseDomain}
 data = urllib.urlencode(data)
-f = urllib.urlopen("https://www.cloudflare.com/api_json.html", data)
-recloadall=f.read()
-#print recloadall
-recloadall=recloadall[0:recloadall.find('"display_name":"'+str(recordName)+'","type":"'+str(recordType)+'"')]
-recordID=recloadall[recloadall.rfind("rec_id")+9:recloadall.find('","',recloadall.rfind("rec_id")+9)]
+try:
+  f = urllib.urlopen("https://www.cloudflare.com/api_json.html", data)
 
-if recordID.find(':"error"')>-1:
-        recordID=recordID[recordID.find('"msg":"')+6:recordID.find(",",recordID.find('"msg":"')+3)]
-print "CF Record:",recordID
-
-#Get your current device IP Address
-f = urllib.urlopen("http://ip-api.com/xml")
-ipe=f.read()
-ip=ipe[ipe.find("<query><![CDATA[")+16:ipe.rfind("]]>",ipe.find("<query><![CDATA["))].strip()
-print "IP Address:",ip
-
-#Update with Cloudflare
-data={'a': 'rec_edit','tkn': cloudflareAPIkey,'id': recordID,'email': cloudflareEmail,'z': baseDomain,'type': recordType,'name': recordName,'content': ip,'service_mode': '0','ttl': '1'}
-data = urllib.urlencode(data)
-f = urllib.urlopen("https://www.cloudflare.com/api_json.html", data)
-response=f.read()
-#print response
-print "Update:",response[response.find('result":')+9:response.find(',',response.find('result":')+3)-1]
+  CFR = json.loads(f.read())
+  matching_CF_recids = map((lambda r: r['rec_id']), filter((lambda o: o['display_name'] == recordName and o['type'] == recordType),CFR['response']['recs']['objs']))
+  if len(matching_CF_recids) == 0 or matching_CF_recids[0] == "error" or CFR['msg'] is not None:
+    recordID = CFR['msg']
+  else:
+    recordID = matching_CF_recids[0]
+    
+  print "CF Record:",recordID
+  
+  f = urllib.urlopen("http://ip-api.com/json")
+  ip = json.loads(f.read())['query']
+  print "IP Address:",ip
+  
+  #Update with Cloudflare
+  data={'a': 'rec_edit','tkn': cloudflareAPIkey,'id': recordID,'email': cloudflareEmail,'z': baseDomain,'type': recordType,'name': recordName,'content': ip,'service_mode': '0','ttl': '1'}
+  data = urllib.urlencode(data)
+  f = urllib.urlopen("https://www.cloudflare.com/api_json.html", data)
+  CFR = json.loads(f.read())
+  print "Update:", CFR['result']
+  
+except:
+  sys.exit(-1)
